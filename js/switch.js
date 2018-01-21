727f29520063bd5192a4e2c9cde9d0354551c9bf
Meg Scholl <meghan.e.scholl@gmail.com>
2018-01-21 16:06:10 -0600

    added files

---
+++ js/switch.js
@@ -0,0 +1,26 @@
+var counter = 0;
+var outputElement = document.getElementById("merp");
+
+// counter += 1;
+// outputElement.innerHTML += counter;
+
+// counter += 2;
+// outputElement.innerHTML += counter; 
+
+// counter += 3;
+// outputElement.innerHTML += counter; 
+
+// counter += 4;
+// outputElement.innerHTML += counter; 
+
+// counter += 5;
+// outputElement.innerHTML += counter;
+
+
+function incrementCounterAndOutput () {
+    counter += 1;
+    outputElement.innerHTML += counter;
+
+}
+
+incrementCounterAndOutput();
